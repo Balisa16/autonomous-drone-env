ad6178cf9b1b3b2b0b5680acd01b90b8fb405631
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-02-02 14:42:38 +0700

    Update

--- source/core.sh
+++ source/core.sh
@@ -199,7 +199,7 @@ ins_simulation()
     echo -e "sim()\n{\n\troslaunch simulation sitl.launch\n}\n" >> "$sitl_file"
     echo -e "sitl()\n{\n\tcd $home_dir/drone/ardupilot/ArduCopter\n\t. ~/.profile\n\tsim_vehicle.py -v ArduCopter -f gazebo-iris --console\n}\n" >> "$sitl_file"
     echo -e "apm_sim()\n{\n\troslaunch simulation apm.launch\n}\n" >> "$sitl_file"
-    echo -e "apm()\n{\n\troslaunch mavros apm.launch\n}\n" >> "$sitl_file"
+    echo -e "apm()\n{\n\troslaunch mavros apm.launch\n}" >> "$sitl_file"
 
     sudo chmod +x "$sitl_file"
 
